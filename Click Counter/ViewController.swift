d1edcd3918072324a5f6b5307d367e4db287b8e5
amnanandy <17368268+amnanandy@users.noreply.github.com>
2018-10-25 18:40:23 -0500

    Initial Commit

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Anna Mandy on 10/25/18.
+//  Copyright © 2018 Anna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
